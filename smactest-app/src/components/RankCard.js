87b198f2a4feab80fda85bb45c57a40d78b657a0
Ethan Tiger Wakefield <92559675+ethantiger@users.noreply.github.com>
2022-05-17 19:48:23 -0400

    Added Home page cards

--- smactest-app/src/components/RankCard.js
+++ smactest-app/src/components/RankCard.js
@@ -1,21 +1,58 @@
-import { AnimatePresence, motion } from 'framer-motion'
+import { motion } from 'framer-motion'
 import { useRank } from '../hooks/useRank'
+import './RankCard.css'
+import { useNavigate } from 'react-router-dom'
+import { CloseButton } from 'react-bootstrap'
 
-export default function RankCard({ranks, title}) {
-    const { changeGroup, group } = useRank()
+export default function RankCard({ranks, title, bg, color}) {
+    const { changeGroup, changeRank, group } = useRank()
+    const navigate = useNavigate()
 
     return (
         <>
-            <motion.div layoutId={title} onClick={() => changeGroup(title)} className="card-container">
-                <div className="card-title">{title}</div>
+            <motion.div className="card-container"  variants={cardVariants} style={{backgroundColor: `${bg}`}} onClick={() => changeGroup(title)}>
+                {group === title ? 
+                <div>
+                    <CloseButton className="card-button" 
+                        onClick={(e) => {
+                            e.stopPropagation()
+                            changeGroup(null)
+                        }}/>
+                    {ranks.map((rank) => (
+                            <motion.div key={rank.name} onClick={() => {
+                                changeRank(rank.name)
+                                navigate('/questions')
+                            }}
+                            variants={rankVariants}
+                            >
+                                <p className='sash' style={{backgroundColor: `${rank.color}`, color: `${color}`}}>
+                                    {rank.name}
+                                    {rank.stripe && <span className='stripe' style={{backgroundColor: `${rank.stripe}`, right: '20px'}}/>}
+                                    {rank.stripe && rank.stripeNum === 2 && <span className='stripe' style={{backgroundColor: `${rank.stripe}`, right: `${20 * rank.stripeNum + 10}px`}}/>}
+                                </p>
+                               
+                            </motion.div>
+                    ))}
+                </div> : <div className="card-title">{title}</div>}
             </motion.div>
-            <AnimatePresence>
-                {group === title && (
-                    <motion.div layoutId={group} onClick={() => changeGroup(null)}>
-                        <h2>{group}</h2>
-                    </motion.div>
-                )}
-            </AnimatePresence>
         </>
     )
+}
+
+const cardVariants = {
+    hidden: {
+      scale:0,
+    },
+    visible: {
+      scale:1,
+    },
+  }
+
+const rankVariants = {
+    hidden: {
+        x: '-100vw'
+    },
+    visible: {
+        x:0,
+    }
 }
--- smactest-app/src/pages/Home.js
+++ smactest-app/src/pages/Home.js
@@ -1,12 +1,58 @@
 import RankCard from "../components/RankCard"
+import { motion } from "framer-motion"
+
+const Basics = {
+  title: 'Basic',
+  ranks: [{name: 'White', color: 'white', stripe: false}, {name: 'Yellow', color:'#ffeb3b', stripe: false}, {name: 'Orange', color: '#ffc107', stripe: false}],
+  bg: 'orange',
+  color: 'black'
+}
+
+const Level1 = {
+  title: 'Level 1',
+  ranks: [{name: 'Purple', color: '#673ab7', stripe: false}, {name: 'Blue', color: '#2196f3', stripe: false}, {name: 'Blue with Stripe', color: '#2196f3', stripe: '#008b02', stripeNum: 1}],
+  bg: '#00bcd4',
+  color: 'white'
+}
+
+const Level2 = {
+  title: 'Level 2',
+  ranks: [{name: 'Green', color: '#008b02', stripe: false}, {name: 'Green with Stripe', color: '#008b02', stripe: '#5d4037', stripeNum: 1}, {name: 'Green with 2 Stripes', color: '#008b02', stripe: '#5d4037', stripeNum: 2}],
+  bg: '#4caf50',
+  color: 'white'
+}
+
+const Level3 = {
+  title: 'Level 3',
+  ranks: [{name: 'Brown', color: '#5d4037', stripe: false}, {name: 'Brown with Stripe', color: '#5d4037', stripe: 'black', stripeNum: 1}, {name: 'Brown with 2 Stripes', color: '#5d4037', stripe: 'black', stripeNum: 2}],
+  bg: '#795548',
+  color: 'white'
+}
+
+const allRanks = [Basics, Level1, Level2, Level3]
+
 
 export default function Home() {
   return (
     <div>
       <h1>Home</h1>
-      <RankCard title={'hello'}/>
-      <RankCard title={'bye'}/>
+      <motion.div variants={cardVariants} initial='hidden' animate='visible'>
+        {allRanks.map((rank) => (
+          <RankCard key={rank.title} title={rank.title} ranks={rank.ranks} bg={rank.bg} color={rank.color} />
+        ))}
+      </motion.div>
     </div>
-    
   )
+}
+
+const cardVariants = {
+  hidden: {
+    opacity: 0,
+  },
+  visible: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.2
+    }
+  }
 }
