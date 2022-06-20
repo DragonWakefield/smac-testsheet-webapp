9c0e028548ef8acc4613ad329b04b745091dbbfb
ethantiger <ethanwakefield007@gmail.com>
2022-06-20 13:38:17 -0400

    Set up firebase

---
+++ smactest-app/src/firebase/config.js
@@ -0,0 +1,17 @@
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCziWB_EnvsB6mkKdXKxP0zK9IBx0uv69I",
+    authDomain: "smac-testsheet.firebaseapp.com",
+    projectId: "smac-testsheet",
+    storageBucket: "smac-testsheet.appspot.com",
+    messagingSenderId: "1045729946105",
+    appId: "1:1045729946105:web:56d83fdc0ae17c6cc6b528"
+  };
+
+  initializeApp(firebaseConfig)
+
+  const db = getFirestore()
+
+  export { db }
