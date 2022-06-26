4ce147f6bf63e24cc62bc312cf0876916de18025
Daniel Dragon Wakefield <dwakefie@uwo.ca>
2022-06-26 16:28:46 -0400

    Added delete button

--- smactest-app/src/components/testData.js
+++ smactest-app/src/components/testData.js
@@ -1,19 +1,24 @@
-import {Card} from "react-bootstrap";
+import {Card, Button} from "react-bootstrap";
 import FillPDF from "../components/pdfFiller";
+import {useFirestore} from "../hooks/useFirestore";
 import "../components/testData.css";
 const TestData = (props)=>{
-    
+    const {deleteDocument} = useFirestore("sheets");
     function downloadPdf(){
         console.log(props.createdAt.toDate());
         FillPDF(props);
     }
 
+    function deleteData(){
+        deleteDocument(props.id);
+    }
     return(
         <div>
             <Card className="mb-3" style={{margin:"auto", width: "40%"}} onClick={downloadPdf}>
                 <Card.Body className={props.Downloaded ? "dl-true row no-gutters": "dl-false row no-gutters"}>
                     <Card.Title> {props.Name}</Card.Title>
                     <Card.Text><b>Rank:</b> {props.Rank} <b className="ms-5">Private Test:</b> {props.PTest.toString()}</Card.Text>
+                    <Button id="deleteButton" onClick={deleteData}>Delete Sheet</Button>
                 </Card.Body>
             </Card>
             
